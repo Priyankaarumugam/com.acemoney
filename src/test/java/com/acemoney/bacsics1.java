0645aabc6659429519b29269b359b313d15af82b
Priyankaarumugam <81764731+Priyankaarumugam@users.noreply.github.com>
2021-04-07 15:10:58 +0530

    update basicclass

---
+++ src/test/java/com/acemoney/bacsics1.java
@@ -0,0 +1,23 @@
+package com.acemoney;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class bacsics1 {
+	
+	@BeforeTest
+	public void setUp()
+	{
+		
+	}
+	
+	@Test
+	public void verifyUrl()
+	{
+		System.out.println("Test1");
+	}
+	
+
+
+}
